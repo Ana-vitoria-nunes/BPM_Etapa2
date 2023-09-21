58b7edf65298695ecf1b74591f8767a8ba9c5ab0
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-09-21 11:47:59 -0300

    Atualização

--- src/main/java/org/example/Desafio3/Fibonacci.java
+++ src/main/java/org/example/Desafio3/Fibonacci.java
@@ -3,10 +3,22 @@ package org.example.Desafio3;
 public class Fibonacci {
 
     public long  mostrarFibonacciNumero(int num){
-        if (num < 2) {
-            return num;
+        if (num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
         } else {
-            return mostrarFibonacciNumero(num - 1) + mostrarFibonacciNumero(num - 2);
+            long fib1 = 0;
+            long fib2 = 1;
+            long fibAtual = 0;
+
+            for (int i = 2; i <= num; i++) {
+                fibAtual = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fibAtual;
+            }
+
+            return fibAtual;
         }
     }
 }
